3a002f5170d92255599748c1c1e407dbe37b32e7
Elliot Betancourt <elliot@elliotbetancourt.com>
2023-11-27 14:38:07 +0100

    update for personal site

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Elliot Betancourts Personal Website',
+  author: 'Elliot Betancourt',
+  headerTitle: 'Elliot Betancourt',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://elliotbetancourt.com',
+  siteRepo: 'https://github.com/ebetancourt/elliotbetancourt.com',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'elliot@elliotbetancourt.com',
+  github: 'https://github.com/ebetancourt',
+  twitter: 'https://twitter.com/ebetan',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/elliotbetancourt',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
